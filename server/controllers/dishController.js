7e9a8360486cb6015d782bfd2c3b429f7c792d70
Nhung <hongnhungpham130995@gmail.com>
2023-09-20 15:20:31 +0200

    function getARandomDish for the Random Dish button

--- server/controllers/dishController.js
+++ server/controllers/dishController.js
@@ -5,8 +5,28 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const apiKey = 'b81e42a35a164c749f93dae5d78f08b6';
 
+//for the Random Dish button
 exports.getARandomDish = async (req,res) => {
+  try {
+    const recipeResponse = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}`);
+    const randomRecipe = recipeResponse.data.recipes[0];
+    const {title, image, summary, instructions} = randomRecipe;
+    const $ = cheerio.load(summary);
+    const plainTextSummary = $.text();
+    const ins = cheerio.load(instructions);
+    const plainTextInstructions = ins.text();
 
+    const randomDish = {
+      title,
+      image,
+      summary: plainTextSummary,
+      instructions: plainTextInstructions,
+    }
+    res.status(200).json(randomDish);
+  } catch (err) {
+    console.log('Error', err);
+    res.status(500).send('Internal Server Error');
+  }
 };
 
 //this is for my main page, it should be showing 3 random dishes
--- server/router.js
+++ server/router.js
@@ -4,6 +4,6 @@ const router = require('express').Router();
 const dish = require('./controllers/dishController');
 
 router.get('/random-dishes', dish.getThreeRandomDishes);
-
+router.get('/a-random-dish', dish.getARandomDish);
 
 module.exports = router;
